5acc0df2bf7f35b99277ecc5b7d716610e663b2c
jeffzh4ng <zhang.jeffreyd@gmail.com>
2026-08-18 17:47:53 -0400

    borscht: goodbye teeny

--- teeny/dex.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#----------------------------------------------------------------------------
-# Execute a command within a rust-cuda Docker container created with the
-# accompanying `dcr` script.
-#
-# E.g. `./dex cargo build` runs `cargo build` within the container.
-#----------------------------------------------------------------------------
-
-docker exec teenygrad-dev bash -lc "$*"
--- teeny/teenygrad/eagker.py
+++
@@ -1,3 +0,0 @@
-def saxpy(): raise NotImplementedError("todo")
-def gemv(): raise NotImplementedError("todo")
-def gemm(): raise NotImplementedError("todo")
